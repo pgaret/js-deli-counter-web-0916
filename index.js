5c421da7f25afb709a23b86994e97b1fe37dd2d4
pgaret <peregringaret@gmail.com>
2016-11-03 12:51:32 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(line, name){
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line){
+  if (line.length == 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${line.shift()}.`;
+  }
+}
+
+function currentLine(line){
+  if (line.length == 0) {
+    return "The line is currently empty."
+  } else {
+    var str = "The line is currently: "
+    for (let i = 0; i < line.length; i++){
+      str += `${i+1}. ${line[i]}`;
+      if (i < line.length - 1) {
+        str += ", ";
+      }
+    }
+  }
+  return str;
+}
